1aa7bf07fa260375cce2f328d0276243b9c150f1
Jon-Erik Johnzon (TorrentKatten) <jone@torrentkatten.se>
2022-01-10 16:43:11 +0100

    wip

--- src/main/kotlin/se/torrentkatten/koliox/Configuration.kt
+++ src/main/kotlin/se/torrentkatten/koliox/Configuration.kt
@@ -3,9 +3,6 @@ package se.torrentkatten.koliox
 import org.apache.kafka.clients.admin.NewTopic
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.core.DefaultKafkaProducerFactory
-import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.core.ProducerFactory
 import se.torrentkatten.koliox.stuff.Topics
 
 
@@ -15,7 +12,4 @@ open class Configuration {
     open fun kolioxTopic(): NewTopic {
         return NewTopic(Topics.kolioxTopic, 1, 1.toShort())
     }
-
-
-
 }
